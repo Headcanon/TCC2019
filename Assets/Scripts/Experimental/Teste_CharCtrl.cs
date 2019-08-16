0d40b535ec311f8e346b67b1fe00ac37c7b390bc
Lucas <bouggroug@hotmail.com>
2019-08-16 15:45:53 -0300

    Experimentando com character controller - L

---
+++ Assets/Scripts/Experimental/Teste_CharCtrl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste_ChangeAxis : MonoBehaviour
+{
+    public Transform[] todosOsPontos;
+    List<Transform> pontosAtivos;
+
+    #region CharCtrl
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+
+        pontosAtivos.Add(todosOsPontos[0]);
+        pontosAtivos.Add(todosOsPontos[1]);
+        pontosAtivos.Add(todosOsPontos[2]);
+    }
+    #endregion
+
+    void Update()
+    {
+        #region CharCtrl
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = transform.right * Input.GetAxis("Horizontal");
+
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+        #endregion
+
+
+        if (Vector3.Distance(pontosAtivos[0].position, transform.position) < Vector3.Distance(pontosAtivos[1].position, transform.position))
+        {
+            transform.parent = pontosAtivos[0];
+        }
+        else if (Vector3.Distance(pontosAtivos[2].position, transform.position) < Vector3.Distance(pontosAtivos[1].position, transform.position))
+        {
+            transform.parent = pontosAtivos[2];
+        }
+
+
+
+
+        for (int i =0; i< pontosAtivos.Count; i++)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ChrCtrl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChrCtrl : MonoBehaviour
+{
+    #region CharCtrl
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    #endregion
+
+    public GameObject ashModel;
+    Animator anim;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        anim = ashModel.GetComponent<Animator>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        #region CharCtrl
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = transform.right * horizontal;
+
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        //else if (!characterController.isGrounded)
+        //{
+
+        //    moveDirection = transform.right * horizontal;
+
+        //    moveDirection *= speed;
+        //}
+
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        ashModel.transform.rotation = Quaternion.Euler(0, 180 - 90 * horizontal, 0);
+        if (anim != null)
+        {
+            anim.SetFloat("Vel", Mathf.Abs(horizontal));
+        }
+        #endregion
+    }
+}
