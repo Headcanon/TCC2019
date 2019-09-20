7f4ac3e7da82e8bbe84de8a76f0601877a3ebf49
Pipilson <pietro.varraschim@hotmail.com>
2019-09-20 12:26:04 -0300

    Script 'Save_Pipilson' criado.

---
+++ Assets/Scripts/Experimental/Save_Pipilson.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Save_Pipilson : MonoBehaviour
+{
+    public string nome;
+    public Vector3 posicao;
+    string json;
+
+    void Start()
+    {
+        //checkpoint
+        //moedas
+        //combustivel
+        //hp
+        //puzzles
+
+        if (PlayerPrefs.HasKey(name))
+        {
+            json = PlayerPrefs.GetString(name);
+            JsonUtility.FromJsonOverwrite(json, this);
+            transform.position = posicao;
+            name = nome;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            posicao = transform.position;
+            nome = name;
+            json = JsonUtility.ToJson(this);
+            PlayerPrefs.SetString(nome, json);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            //JsonUtility.FromJsonOverwrite(json, this);
+            //transform.position = posicao;
+            //name = nome;
+            //JsonUtility.FromJsonOverwrite(json, this);
+
+        }
+    }
+}
