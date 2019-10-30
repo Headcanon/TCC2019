7a8722a9fdf35eef482fa0536fe1d815d5397a1d
Headcanon <44294291+Headcanon@users.noreply.github.com>
2019-10-30 17:28:06 -0200

    Pecas prefabs e animacoes finais implementadas - L

--- Assets/Scripts/NewCheckpoint.cs
+++ Assets/Scripts/NewCheckpoint.cs
@@ -8,9 +8,15 @@ public class NewCheckpoint : MonoBehaviour
     Vida pl;
     Save_Pipilson save;
 
+    private void Start()
+    {
+        save = GameObject.Find("SaveManager").GetComponent<Save_Pipilson>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         pl = other.GetComponent<Vida>();
+
         if(pl != null)
         {
             //pl.spawnPoint = gameObject.transform;
--- Assets/Scripts/PlatformSystem/PlataformaPisavelSimples.cs
+++ Assets/Scripts/PlatformSystem/PlataformaPisavelSimples.cs
@@ -23,17 +23,17 @@ public class PlataformaPisavelSimples : MonoBehaviour
         if (goX)
         {
             // Magia da matemática que calcula a posição X
-            x = Mathf.Sin(Time.time * velocity) * distance;
+            x = Mathf.Sin(Time.fixedTime * velocity) * distance;
         }
         if (goY)
         {
             // Magia da matemática que calcula a posição Y
-            y = Mathf.Sin(Time.time * velocity) * distance;
+            y = Mathf.Sin(Time.fixedTime * velocity) * distance;
         }
         if (goZ)
         {
             // Magia da matemática que calcula a posição Z
-            z = Mathf.Sin(Time.time * velocity) * distance;
+            z = Mathf.Sin(Time.fixedTime * velocity) * distance;
         }
 
 
