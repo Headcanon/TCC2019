d000ecf36b954250ccfc71a2b75f4fa1a62c414d
Headcanon <44294291+Headcanon@users.noreply.github.com>
2019-11-07 16:46:12 -0200

    Camera Adaptavel implementada e pastas trocadas - L

---
+++ Assets/DollyManager.cs
@@ -0,0 +1,42 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollyManager : MonoBehaviour
+{
+    //public CinemachineSmoothPath csp;
+    private CinemachineTrackedDolly ctd;
+    private Animator anim;
+    public NextCam[] cameras;
+
+    [System.Serializable]
+    public struct NextCam
+    {
+        public string cameraAtual;
+        public float posicaoSaida;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ctd = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            if (ctd.m_PathPosition > cameras[i].posicaoSaida && ctd.m_PathPosition < cameras[i + 1].posicaoSaida)
+            {
+                anim.SetTrigger(cameras[i+1].cameraAtual);
+            }
+            else if (ctd.m_PathPosition < cameras[i].posicaoSaida && ctd.m_PathPosition > cameras[i - 1].posicaoSaida)
+            {
+                anim.SetTrigger(cameras[i].cameraAtual);
+            }
+        }
+    }
+}
