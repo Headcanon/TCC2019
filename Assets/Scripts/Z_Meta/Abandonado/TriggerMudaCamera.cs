b7f67436af5a23eaaf6093b8fdaac878a5b378e7
Headcanon <44294291+Headcanon@users.noreply.github.com>
2019-11-08 11:56:00 -0200

    Todas as câmeras implementadas - L

---
+++ Assets/Scripts/Z_Meta/Abandonado/TriggerMudaCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Muda o angulo de camera quando player passa por esse trigger */
+public class TriggerMudaCamera : MonoBehaviour
+{
+    public Animator cm;
+    public string camera;
+
+    private void OnTriggerEnter(Collider target)
+    {
+        if (target.CompareTag("Player"))
+        {
+            cm.SetTrigger(camera);
+        }
+    }
+}
