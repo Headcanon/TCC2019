c580b8898c7aec6d3e228d9682ad0fd2d1998f4d
Pipilson <pietro.varraschim@hotmail.com>
2019-11-11 12:34:11 -0200

    Deletei tudo

--- Assets/Scripts/PublicEnums.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/* Enums publicas para serem usadas em outros scripts */
-public enum TipoGranada { GELO, DESTROY };
