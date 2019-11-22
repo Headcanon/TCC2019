9ce7be5a9dd99860af57d09255bcdd9d468cb299
Headcanon <44294291+Headcanon@users.noreply.github.com>
2019-11-22 13:29:51 -0200

    Moedas girantes e Outline das pecas - P

--- Assets/Scripts/Itens/Moeda.cs
+++ Assets/Scripts/Itens/Moeda.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Moeda : MonoBehaviour
 {
     public int pontos = 5;
+    public Vector3 rotacao;
 
     private Pontuacao gm;
 
     void Start()
     {
         gm = FindObjectOfType<Pontuacao>();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(rotacao);
     }
 
     private void OnTriggerEnter(Collider other)
